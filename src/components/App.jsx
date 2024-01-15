bb489ab0255c904e1dbeb2b7f236c301a35d2f46
adiutant <mikolaj@marasek.eu>
2024-01-15 13:16:03 +0100

    form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 function App() {
+    const [show,setShow] = useState('')
+    const inputRef = useRef(null)
+    const onSubmitHandle = (e) => {
+        e.preventDefault()
+        setShow(inputRef.current.value)
+    }
 
-  return (
+
+        return (
     <>
-        test
+        <form onSubmit={onSubmitHandle}>
+            <input ref={inputRef} placeholder={'search for meal'}/>
+            <button type="submit">search</button>
+        </form>
+        <div>{show}</div>
     </>
   )
 }
