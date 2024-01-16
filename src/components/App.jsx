fdccdf1aa7064ebcc21a2fcb9e27f53d1ca06509
adiutant <mikolaj@marasek.eu>
2024-01-17 00:00:52 +0100

    maping searched dishes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,43 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 function App() {
-    const [inputValue,setInputValue] = useState('')
+    const [inputValue, setInputValue] = useState('')
     const inputRef = useRef(null)
-    let data = {}
-
+    let [data, setData] = useState(null)
     async function request() {
         try {
-            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
-            data = await response.json()
+            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`);
+            setData(await response.json());
             console.log(data)
-
         }catch (err){
-            console.error(err)
+            console.error(err);
         }
     }
 
 
     const onSubmitHandle = (e) => {
-        e.preventDefault()
-        setInputValue(inputRef.current.value)
-        request()
-    }
+        e.preventDefault();
+        setInputValue(inputRef.current.value);
 
+
+
+    }
+    useEffect(() =>{
+        request()
+
+
+    },[inputValue])
 
         return (
     <>
-        <form onSubmit={onSubmitHandle}>
+
+        <form name={'form'} onSubmit={onSubmitHandle}>
             <input ref={inputRef} placeholder={'search for meal'}/>
             <button type="submit">search</button>
         </form>
-
+        {data && data.meals && data.meals.map( (meal) => (
+            <p key={meal.idMeal}>{meal.strMeal}</p>
+        ))}
+        {data && data.meals === null ? <p> no dishes </p> : ""}
 
 
 
