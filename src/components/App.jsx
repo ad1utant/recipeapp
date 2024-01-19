a940c938862a29c5b0f6b034dc16f7455f6e77e1
adiutant <mikolaj@marasek.eu>
2024-01-19 13:06:09 +0100

    going to revert

--- src/components/App.jsx
+++
@@ -1,7 +0,0 @@
-import '/src/styles/Meals.css'
-import Meals from "./Meals.jsx";
-function App(){
-return(
-    <Meals/>
-    )}
-export default App
--- src/components/Meal.jsx
+++
@@ -1,8 +0,0 @@
-function Meal(props){
-    const data  = props.data
-
-    return(
-        <div>{data}</div>
-    )
-}
-export default Meal
