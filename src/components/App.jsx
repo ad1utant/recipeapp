8903c23e29bc1beec946b2071af30b569e47f061
adiutant <mikolaj@marasek.eu>
2024-01-19 13:01:48 +0100

    Revert "nav component added"
    This reverts commit 72bf47f3805ca7d37a013ae4e5b43d3dc3846278.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,7 @@
 import '/src/styles/Meals.css'
 import Meals from "./Meals.jsx";
-import Nav from "./Nav.jsx";
 function App(){
 return(
-    <div>
-        <Nav/>
-        <Meals/>
-    </div>
-        )}
+    <Meals/>
+    )}
 export default App
