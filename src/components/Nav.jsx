be71932bbca06659e93ae6169198a71f2721cac0
adiutant <mikolaj@marasek.eu>
2024-01-19 14:34:10 +0100

    basic nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,19 @@
+import {Link} from "react-router-dom";
+import '/src/styles/Nav.css'
 function Nav(){
     return(
         <nav>
-            hello
+            <div className={'nav-items'}>
+                <div className={'first-child'}>
+                    <Link to={'/categorise'}>Categories</Link>
+                    <Link to={'/random'}>Random Meal</Link>
+                    <Link to={'/search'}>Search</Link>
+                </div>
+                <div className={'last-child'}>
+                    <Link to={'/about'}>About</Link>
+                    <Link to={'/login'}>Login</Link>
+                </div>
+            </div>
         </nav>
     )
 }
