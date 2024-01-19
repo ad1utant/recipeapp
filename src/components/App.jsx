c4bfa45f50621fe2b9bc06aa0474cf90483474d4
adiutant <mikolaj@marasek.eu>
2024-01-19 12:58:47 +0100

    Revert "some issues to fix realted to random pagge request"
    This reverts commit 586484d895741496223da9ef14be9aff66e887bc.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,11 @@
 import '/src/styles/Meals.css'
 import Meals from "./Meals.jsx";
 import Nav from "./Nav.jsx";
-import {Route, Routes} from "react-router";
-import Random from "./Random.jsx";
 function App(){
 return(
     <div>
         <Nav/>
-        <Routes>
-            <Route path={'/'} element={<Meals/>}/>
-            <Route path={'/meals'} element={<Meals/>}/>
-            <Route path={'/random'} element={<Random/>}/>
-        </Routes>
+        <Meals/>
     </div>
         )}
 export default App
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,8 @@
 import '../styles/Nav.css'
-import {NavLink} from "react-router-dom";
 function Nav(){
     return(
-        <div className={'container'}>
-            <NavLink to={'/meals'}>
-                Search for meal
-            </NavLink>
-            <NavLink to={'/random'}>
-                Random meal
-            </NavLink>
-
+        <div>
+            nav
         </div>
     )
 }
