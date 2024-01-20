c70e61562567d1c4961d47d0359b5a4286c785d9
adiutant <mikolaj@marasek.eu>
2024-01-21 00:22:29 +0100

    add meal component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import '/src/styles/App.css'
 import Nav from "./Nav.jsx";
 import List from "./List.jsx";
-
+import Meal from "./Meal.jsx";
 function App() {
 return(
     <div>
         <Nav/>
+        <Meal/>
         <List/>
     </div>
 )
---
+++ src/components/Meal.jsx
@@ -0,0 +1,6 @@
+function Meal(){
+    return(
+        <h1>meal proporties</h1>
+    )
+}
+export default Meal
