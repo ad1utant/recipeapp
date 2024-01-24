66b115a36bcb899aa45c9263df76ae9e48cd5368
adiutant <mikolaj@marasek.eu>
2024-01-24 11:06:42 +0100

    destructuring ingredients

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -1,17 +1,22 @@
 import {useParams} from "react-router";
 import {useEffect, useState} from "react";
 
+let listIngredients = [];
 function Meal(){
+
     const {idMeal} = useParams()
     const [details,setDetails] = useState()
 
     useEffect( () => {
         const  fetches = async () => {
         try {
-                const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
-                const data = await response.json()
-                setDetails(data)
+            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+            const data = await response.json()
+            const {strIngredient1} = data.meals[0]
+            console.log(strIngredient1)
+            setDetails(data)
             console.log(data)
+
         }catch (err){
                 console.error(err)
             }}
@@ -24,6 +29,8 @@ function Meal(){
 
         <div>
             {details ? <h1>{details.meals[0].strMeal}</h1> : null}
+            <h2>Ingredients</h2>
+
             {details ? <img src={details.meals[0].strMealThumb}/> : null}
 
         </div>
