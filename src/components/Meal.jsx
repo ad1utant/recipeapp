4768777a0a17db4c042ef368452b9751336d234a
adiutant <mikolaj@marasek.eu>
2024-01-25 23:54:32 +0100

    add instructions

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -2,32 +2,34 @@ import {useParams} from "react-router";
 import {useEffect, useState} from "react";
 
 function Meal(){
-    const {idMeal} = useParams()
-    const [details,setDetails] = useState()
+    const {idMeal} = useParams();
+    const [details,setDetails] = useState();
     const [ingredients,setIngredients] = useState([]);
-    const [measures,setMeasures] = useState([])
-
+    const [measures,setMeasures] = useState([]);
+    const [instructions, setInstructions] = useState();
     useEffect( () => {
         const  fetches = async () => {
         try {
-            let arrayIngredients = []
-            let arrayMeasures = []
-            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
-            const data = await response.json()
+            let arrayIngredients = [];
+            let arrayMeasures = [];
+            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`);
+            const data = await response.json();
             for(let i = 0 ; i<=20 ; i++){
-                arrayIngredients.push(data.meals[0][`strIngredient${i}`])
-                arrayMeasures.push(data.meals[0][`strMeasure${i}`])
+                arrayIngredients.push(data.meals[0][`strIngredient${i}`]);
+                arrayMeasures.push(data.meals[0][`strMeasure${i}`]);
             }
-            console.log(ingredients)
-            setIngredients(arrayIngredients)
-            setMeasures(arrayMeasures)
-            setDetails(data)
-            console.log(data)
+            const {strInstructions} = data.meals[0];
+            setInstructions(strInstructions);
+            console.log(ingredients);
+            setIngredients(arrayIngredients);
+            setMeasures(arrayMeasures);
+            setDetails(data);
+            console.log(data);
 
         }catch (err){
-                console.error(err)
+                console.error(err);
             }}
-            fetches()
+            fetches();
 
     },[idMeal])
 
@@ -36,6 +38,8 @@ function Meal(){
 
         <div>
             {details ? <h1>{details.meals[0].strMeal}</h1> : null}
+            <h2>Instructions</h2>
+            <p>{instructions}</p>
             <h2>Ingredients</h2>
             <ul>
             {ingredients.map((ingredient,index) => (
@@ -44,6 +48,7 @@ function Meal(){
 
             {details ? <img src={details.meals[0].strMealThumb}/> : null}
             </ul>
+
         </div>
     )
 }
