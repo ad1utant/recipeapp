5ab5ee71df2de79d4236130fbe0796ab4b7c92b3
adiutant <mikolaj@marasek.eu>
2024-01-25 11:05:33 +0100

    ingredients in meal page

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -1,22 +1,22 @@
 import {useParams} from "react-router";
 import {useEffect, useState} from "react";
 
-let listIngredients = [];
 function Meal(){
-    let array = [];
     const {idMeal} = useParams()
     const [details,setDetails] = useState()
+    let [ingredients,setIngredients] = useState([]);
 
     useEffect( () => {
         const  fetches = async () => {
         try {
+            let array = []
             const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
             const data = await response.json()
-            array = []
             for(let i = 0 ; i<=20 ; i++){
                 array.push(data.meals[0][`strIngredient${i}`])
             }
-            console.log(array)
+            console.log(ingredients)
+            setIngredients(array)
             setDetails(data)
             console.log(data)
 
@@ -33,6 +33,9 @@ function Meal(){
         <div>
             {details ? <h1>{details.meals[0].strMeal}</h1> : null}
             <h2>Ingredients</h2>
+            {ingredients.map((ingredient,index) => (
+                <h3 key={index}>{ingredient}</h3>
+            ))}
 
             {details ? <img src={details.meals[0].strMealThumb}/> : null}
 
