fa687eb761362ec3518a1f2856b98ebfd56685bc
adiutant <mikolaj@marasek.eu>
2024-01-25 13:42:39 +0100

    measures ingredients

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -4,19 +4,23 @@ import {useEffect, useState} from "react";
 function Meal(){
     const {idMeal} = useParams()
     const [details,setDetails] = useState()
-    let [ingredients,setIngredients] = useState([]);
+    const [ingredients,setIngredients] = useState([]);
+    const [measures,setMeasures] = useState([])
 
     useEffect( () => {
         const  fetches = async () => {
         try {
-            let array = []
+            let arrayIngredients = []
+            let arrayMeasures = []
             const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
             const data = await response.json()
             for(let i = 0 ; i<=20 ; i++){
-                array.push(data.meals[0][`strIngredient${i}`])
+                arrayIngredients.push(data.meals[0][`strIngredient${i}`])
+                arrayMeasures.push(data.meals[0][`strMeasure${i}`])
             }
             console.log(ingredients)
-            setIngredients(array)
+            setIngredients(arrayIngredients)
+            setMeasures(arrayMeasures)
             setDetails(data)
             console.log(data)
 
@@ -35,7 +39,7 @@ function Meal(){
             <h2>Ingredients</h2>
             <ul>
             {ingredients.map((ingredient,index) => (
-                ingredient ? <li key={index}>{ingredient}</li> : null
+                ingredient ? <li key={`${index}ing`}>{ingredient} - {measures[index]}</li> : null
             ))}
 
             {details ? <img src={details.meals[0].strMealThumb}/> : null}
