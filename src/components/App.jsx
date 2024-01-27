39b1f4a703b3f3d4c1e586539cdef8a7aed8dbe7
adiutant <mikolaj@marasek.eu>
2024-01-27 22:09:02 +0100

    add about component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import Meal from "./Meal.jsx";
 import {Route, Routes, Navigate} from "react-router";
 import Categories from "./Categories.jsx";
 import Random from "./Random.jsx";
+import About from "./About.jsx";
 function App() {
 return(
     <div>
@@ -13,6 +14,7 @@ return(
             <Route path={'/'} element={<List/>}/>
             <Route path={"/categories"} element={<Categories/>}/>
             <Route path={"/random"} element={<Random/>}/>
+            <Route path={"/about"} element={<About/>}/>
             <Route path={'/:idMeal'} element={<Meal/>}/>
             <Route path="/search" element={<Navigate to="/" />} />
         </Routes>
