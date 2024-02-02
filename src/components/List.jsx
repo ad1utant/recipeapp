b10eb0d25c2c5c07df550be1b0ab44fe6e5a9a3e
adiutant <mikolaj@marasek.eu>
2024-02-02 15:09:46 +0100

    change layout a little

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -31,11 +31,13 @@ useEffect(() =>{
 
 return (
     <>
+        <div className={'col-12'}>
         <form className={'d-flex flex-row justify-content-center mt-3'} name={'form'} onSubmit={onSubmitHandle}>
-            <input className={'form-control col-7'} ref={inputRef} placeholder={'search for meal'}/>
+            <input className={'form-control'} ref={inputRef} placeholder={'search for meal'}/>
             <button className={'btn btn-dark'} type="submit">search</button>
         </form>
-        <div className={'d-flex flex-wrap col-10 m-3 justify-content-center mx-auto'}>
+        </div>
+        <div className={'d-flex flex-wrap col-12 m-3 justify-content-center mx-auto'}>
             {data && data.meals && data.meals.map( (meal) => (
                 <Link className={' custom-border flex-column p-0 m-3 justify-content-center'} to={meal.idMeal} key={meal.idMeal}>
                         <img className={'image-custom w-100 h-1'} src={meal.strMealThumb}/>
